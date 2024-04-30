e7aa697e17783543689664958661bd5b76899357
Giovanni <giovannicuervo@pm.me>
2024-04-30 16:31:08 -0600

    Added homapage

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  registerMode = false;
+
+  constructor() {}
+
+  ngOninit(): void {}
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+}
