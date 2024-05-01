d9ca304beadd7df0c486b567b74e635061c6f25e
Giovanni <giovannicuervo@pm.me>
2024-04-30 18:16:59 -0600

    64pt1 - routing - components created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,21 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailedComponent } from './members/member-detailed/member-detailed.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailedComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/members/member-detailed/member-detailed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-detailed',
+  templateUrl: './member-detailed.component.html',
+  styleUrls: ['./member-detailed.component.css']
+})
+export class MemberDetailedComponent {
+
+}
