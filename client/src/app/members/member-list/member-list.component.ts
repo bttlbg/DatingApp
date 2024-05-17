f38834fc1e70d067883029471f06bc1f19f57cea
Giovanni <giovannicuervo@pm.me>
2024-05-17 10:55:06 -0600

    155 - Setting up client pagination

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MembersService } from 'src/app/_services/members.service';
 import { Member } from 'src/app/models/member';
+import { Pagination } from 'src/app/models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -9,15 +10,27 @@ import { Member } from 'src/app/models/member';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]> | undefined;
+  /* members$: Observable<Member[]> | undefined; */
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  pagenumber = 1;
+  pagesize = 5;
 
   constructor(private memberService: MembersService) { }
   ngOnInit(): void {
+    /* this.members$ = this.memberService.getMembers(); */
     this.loadMember();
   }
 
   loadMember() {
-    this.members$ = this.memberService.getMembers();
+    this.memberService.getMembers(this.pagenumber, this.pagesize).subscribe({
+      next: response => {
+        if (response.result && response.pagination) {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      }
+    })
   }
 
 }
