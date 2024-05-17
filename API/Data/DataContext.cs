01bcdac791f3dedfc230bb911febe02cea0652ab
Giovanni <giovannicuervo@pm.me>
2024-05-17 17:32:02 -0600

    171 - Adding the likes entity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,4 +10,28 @@ public class DataContext : DbContext
     }
 
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<UserLike> Likes {get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // likes table
+        builder.Entity<UserLike>()
+            .HasKey(k => new {k.SourceUserId, k.TargetUserId});
+
+        // one user can like another user
+        // one user likes many oter users
+        builder.Entity<UserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikedUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<UserLike>()
+            .HasOne(s => s.LikedUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.TargetUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
