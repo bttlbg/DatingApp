419091b699db378fff73728fcbe44b498872256a
Giovanni <giovannicuervo@pm.me>
2024-05-20 09:51:00 -0600

    175 - Setting up the like funtions in angular app

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { MembersService } from 'src/app/_services/members.service';
 import { Member } from 'src/app/models/member';
 
 @Component({
@@ -9,8 +11,14 @@ import { Member } from 'src/app/models/member';
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private toastr: ToastrService) { }
   ngOnInit(): void {
 
+  }
+
+  addLike(member: Member) {
+    this.memberService.addLike(member.userName).subscribe({
+      next: () => this.toastr.success('You have liked ' + member.knownAs)
+    })
   }
 }
