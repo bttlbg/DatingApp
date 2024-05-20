9a6f06e7d77b3e1c731e42f0fac873ae4634e71e
Giovanni <giovannicuervo@pm.me>
2024-05-20 17:40:28 -0600

    195 - Fixing the photo weirdness

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -14,6 +14,7 @@ export class MessagesComponent implements OnInit {
   container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private messageService: MessageService) {}
   ngOnInit(): void {
@@ -21,10 +22,12 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
       next: response => {
         this.messages = response.result;
         this.pagination = response.pagination;
+        this.loading = false;
       }
     })
   }
