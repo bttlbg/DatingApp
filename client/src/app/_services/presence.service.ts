134e1a8bc8497411eb2d9a2f545fa5b9ed07480f
Giovanni <giovannicuervo@pm.me>
2024-05-22 18:05:07 -0600

    234 - Optimizing the presence

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -31,11 +31,15 @@ export class PresenceService implements OnInit {
     this.hubConnection.start().catch(error => {console.log(error)});
 
     this.hubConnection.on("UserIsOnline", username => {
-      this.toastr.info(username + " has connected");
+      this.onlineUsers$.pipe(take(1)).subscribe({
+        next: usernames => this.onlineUsersSource.next([...usernames, username])
+      })
     });
 
     this.hubConnection.on("UserIsOffline", username => {
-      this.toastr.info(username + " has disconnected");
+      this.onlineUsers$.pipe(take(1)).subscribe({
+        next: usernames => this.onlineUsersSource.next(usernames.filter(x => x !== username))
+      })
     });
 
     this.hubConnection.on("GetOnlineUsers", usernames => this.onlineUsersSource.next(usernames));
